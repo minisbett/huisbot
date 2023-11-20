5d860101d1c486abf0959472722f7603c5e8bb06
minisbett <39670899+minisbett@users.noreply.github.com>
2023-11-20 20:18:08 +0100

    Add osu mod enum

--- huisbot/Enums/HuisScoreSort.cs
+++ huisbot/Enums/HuisScoreSort.cs
@@ -5,112 +5,112 @@
 /// </summary>
 public class HuisScoreSort
 {
-  private static HuisScoreSort[] _all = null!;
+    private static HuisScoreSort[] _all = null!;
 
-  /// <summary>
-  /// Returns all the available sort options.
-  /// </summary>
-  public static HuisScoreSort[] All
-  {
-    get
+    /// <summary>
+    /// Returns all the available sort options.
+    /// </summary>
+    public static HuisScoreSort[] All
     {
-      // Lazy load the array since the property values are not known at compile time.
-      if (_all is null)
-        _all = new HuisScoreSort[]
+        get
         {
+            // Lazy load the array since the property values are not known at compile time.
+            if (_all is null)
+                _all = new HuisScoreSort[]
+                {
           LivePPDesc, LivePPAsc, LocalPPDesc, LocalPPAsc, PPDifferenceDesc, PPDifferenceAsc, AimPPDesc, AimPPAsc, TapPPDesc, TapPPAsc, FLPPDesc, FLPPAsc
-        };
+                };
 
-      return _all;
+            return _all;
+        }
     }
-  }
 
-  /// <summary>
-  /// Sorts by the Live PP of the score in descending order.
-  /// </summary>
-  public static HuisScoreSort LivePPDesc { get; } = new("live_pp", false, "Live PP (Descending)");
+    /// <summary>
+    /// Sorts by the Live PP of the score in descending order.
+    /// </summary>
+    public static HuisScoreSort LivePPDesc { get; } = new("live_pp", false, "Live PP (Descending)");
 
-  /// <summary>
-  /// Sorts by the Live PP of the score in ascending order.
-  /// </summary>
-  public static HuisScoreSort LivePPAsc { get; } = new("live_pp", true, "Live PP (Ascending)");
+    /// <summary>
+    /// Sorts by the Live PP of the score in ascending order.
+    /// </summary>
+    public static HuisScoreSort LivePPAsc { get; } = new("live_pp", true, "Live PP (Ascending)");
 
-  /// <summary>
-  /// Sorts by the Local PP of the score in descending order.
-  /// </summary>
-  public static HuisScoreSort LocalPPDesc { get; } = new("local_pp", false, "Local PP (Descending)");
+    /// <summary>
+    /// Sorts by the Local PP of the score in descending order.
+    /// </summary>
+    public static HuisScoreSort LocalPPDesc { get; } = new("local_pp", false, "Local PP (Descending)");
 
-  /// <summary>
-  /// Sorts by the Local PP of the score in ascending order.
-  /// </summary>
-  public static HuisScoreSort LocalPPAsc { get; } = new("local_pp", true, "Local PP (Ascending)");
+    /// <summary>
+    /// Sorts by the Local PP of the score in ascending order.
+    /// </summary>
+    public static HuisScoreSort LocalPPAsc { get; } = new("local_pp", true, "Local PP (Ascending)");
 
-  /// <summary>
-  /// Sorts by the PP difference of the score in descending order.
-  /// </summary>
-  public static HuisScoreSort PPDifferenceDesc { get; } = new("pp_diff", false, "PP Difference (Descending)");
+    /// <summary>
+    /// Sorts by the PP difference of the score in descending order.
+    /// </summary>
+    public static HuisScoreSort PPDifferenceDesc { get; } = new("pp_diff", false, "PP Difference (Descending)");
 
-  /// <summary>
-  /// Sorts by the PP difference of the score in ascending order.
-  /// </summary>
-  public static HuisScoreSort PPDifferenceAsc { get; } = new("pp_diff", true, "PP Difference (Ascending)");
+    /// <summary>
+    /// Sorts by the PP difference of the score in ascending order.
+    /// </summary>
+    public static HuisScoreSort PPDifferenceAsc { get; } = new("pp_diff", true, "PP Difference (Ascending)");
 
-  /// <summary>
-  /// Sorts by the weighted Aim PP of the score in descending order.
-  /// </summary>
-  public static HuisScoreSort AimPPDesc { get; } = new("aim_pp", false, "Weighted Aim PP (Descending)");
+    /// <summary>
+    /// Sorts by the weighted Aim PP of the score in descending order.
+    /// </summary>
+    public static HuisScoreSort AimPPDesc { get; } = new("aim_pp", false, "Weighted Aim PP (Descending)");
 
-  /// <summary>
-  /// Sorts by the weighted Aim PP of the score in ascending order.
-  /// </summary>
-  public static HuisScoreSort AimPPAsc { get; } = new("aim_pp", true, "Weighted Aim PP (Ascending)");
+    /// <summary>
+    /// Sorts by the weighted Aim PP of the score in ascending order.
+    /// </summary>
+    public static HuisScoreSort AimPPAsc { get; } = new("aim_pp", true, "Weighted Aim PP (Ascending)");
 
-  /// <summary>
-  /// Sorts by the weighted Tap PP of the score in descending order.
-  /// </summary>
-  public static HuisScoreSort TapPPDesc { get; } = new("tap_pp", false, "Weighted Tap PP (Descending)");
+    /// <summary>
+    /// Sorts by the weighted Tap PP of the score in descending order.
+    /// </summary>
+    public static HuisScoreSort TapPPDesc { get; } = new("tap_pp", false, "Weighted Tap PP (Descending)");
 
-  /// <summary>
-  /// Sorts by the weighted Tap PP of the score in ascending order.
-  /// </summary>
-  public static HuisScoreSort TapPPAsc { get; } = new("tap_pp", true, "Weighted Tap PP (Ascending)");
+    /// <summary>
+    /// Sorts by the weighted Tap PP of the score in ascending order.
+    /// </summary>
+    public static HuisScoreSort TapPPAsc { get; } = new("tap_pp", true, "Weighted Tap PP (Ascending)");
 
-  /// <summary>
-  /// Sorts by the weighted FL PP of the score in descending order.
-  /// </summary>
-  public static HuisScoreSort FLPPDesc { get; } = new("fl_pp", false, "Weighted FL PP (Descending)");
+    /// <summary>
+    /// Sorts by the weighted FL PP of the score in descending order.
+    /// </summary>
+    public static HuisScoreSort FLPPDesc { get; } = new("fl_pp", false, "Weighted FL PP (Descending)");
 
-  /// <summary>
-  /// Sorts by the weighted FL PP of the score in ascending order.
-  /// </summary>
-  public static HuisScoreSort FLPPAsc { get; } = new("fl_pp", true, "Weighted FL PP (Ascending)");
+    /// <summary>
+    /// Sorts by the weighted FL PP of the score in ascending order.
+    /// </summary>
+    public static HuisScoreSort FLPPAsc { get; } = new("fl_pp", true, "Weighted FL PP (Ascending)");
 
-  /// <summary>
-  /// An identifier to uniquely identify the combination of sort code and order.
-  /// </summary>
-  public string Id => $"{Code}_{(IsAscending ? "asc" : "desc")}";
+    /// <summary>
+    /// An identifier to uniquely identify the combination of sort code and order.
+    /// </summary>
+    public string Id => $"{Code}_{(IsAscending ? "asc" : "desc")}";
 
-  /// <summary>
-  /// The code used in the Huis API parameter for this sort option.
-  /// </summary>
-  public string Code { get; }
+    /// <summary>
+    /// The code used in the Huis API parameter for this sort option.
+    /// </summary>
+    public string Code { get; }
 
-  /// <summary>
-  /// Bool whether the sort order is ascending or not.
-  /// </summary>
-  public bool IsAscending { get; }
+    /// <summary>
+    /// Bool whether the sort order is ascending or not.
+    /// </summary>
+    public bool IsAscending { get; }
 
-  /// <summary>
-  /// The display name of the sort option.
-  /// </summary>
-  public string DisplayName { get; }
+    /// <summary>
+    /// The display name of the sort option.
+    /// </summary>
+    public string DisplayName { get; }
 
-  private HuisScoreSort(string code, bool ascending, string displayName)
-  {
-    Code = code;
-    IsAscending = ascending;
-    DisplayName = displayName;
-  }
+    private HuisScoreSort(string code, bool ascending, string displayName)
+    {
+        Code = code;
+        IsAscending = ascending;
+        DisplayName = displayName;
+    }
 
-  public override string ToString() => DisplayName;
+    public override string ToString() => DisplayName;
 }
---
+++ huisbot/Enums/OsuMod.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace huisbot.Enums;
+
+/// <summary>
+/// A "fake enum" representing an osu! mod.
+/// </summary>
+/// </summary>
+public class OsuMod
+{
+  private static OsuMod[] _all = null!;
+
+  /// <summary>
+  /// Returns all the available osu! mods.
+  /// </summary>
+  public static OsuMod[] All
+  {
+    get
+    {
+      // Lazy load the array since the property values are not known at compile time.
+      if (_all is null)
+        _all = new OsuMod[]
+        {
+          NoFail, Easy, TouchDevice, Hidden, HardRock, SuddenDeath, DoubleTime, Relax, HalfTime, Nightcore, Flashlight, Auto, SpunOut, AutoPilot
+        };
+
+      return _all;
+    }
+  }
+
+  /// <summary>
+  /// The NoFail osu! mod.
+  /// </summary>
+  public static OsuMod NoFail { get; } = new("NF");
+
+  /// <summary>
+  /// The Easy osu! mod.
+  /// </summary>
+  public static OsuMod Easy { get; } = new("EZ");
+
+  /// <summary>
+  /// The TouchDevice osu! mod.
+  /// </summary>
+  public static OsuMod TouchDevice { get; } = new("TD");
+
+  /// <summary>
+  /// The Hidden osu! mod.
+  /// </summary>
+  public static OsuMod Hidden { get; } = new("HD");
+
+  /// <summary>
+  /// The HardRock osu! mod.
+  /// </summary>
+  public static OsuMod HardRock { get; } = new("HR");
+
+  /// <summary>
+  /// The SuddenDeath osu! mod.
+  /// </summary>
+  public static OsuMod SuddenDeath { get; } = new("SD");
+
+  /// <summary>
+  /// The DoubleTime osu! mod.
+  /// </summary>
+  public static OsuMod DoubleTime { get; } = new("DT");
+
+  /// <summary>
+  /// The Relax osu! mod.
+  /// </summary>
+  public static OsuMod Relax { get; } = new("RX");
+
+  /// <summary>
+  /// The HalfTime osu! mod.
+  /// </summary>
+  public static OsuMod HalfTime { get; } = new("HT");
+
+  /// <summary>
+  /// The Nightcore osu! mod.
+  /// </summary>
+  public static OsuMod Nightcore { get; } = new("NC");
+
+  /// <summary>
+  /// The Flashlight osu! mod.
+  /// </summary>
+  public static OsuMod Flashlight { get; } = new("FL");
+
+  /// <summary>
+  /// The Auto osu! mod.
+  /// </summary>
+  public static OsuMod Auto { get; } = new("AT");
+
+  /// <summary>
+  /// The SpunOut osu! mod.
+  /// </summary>
+  public static OsuMod SpunOut { get; } = new("SO");
+
+  /// <summary>
+  /// The AutoPilot osu! mod.
+  /// </summary>
+  public static OsuMod AutoPilot { get; } = new("AP");
+
+  /// <summary>
+  /// The acronym of the osu! mod.
+  /// </summary>
+  public string Acronym { get; }
+
+  private OsuMod(string acronym)
+  {
+    Acronym = acronym;
+  }
+
+  public override string ToString()
+  {
+    return Acronym;
+  }
+
+  /// <summary>
+  /// Returns the OsuMod array for the specified mod string. All invalid mods are being ignored.
+  /// </summary>
+  /// <param name="modsStr">The mod string.</param>
+  /// <returns>The parsed mods.</returns>
+  public static OsuMod[] Parse(string modsStr)
+  {
+    // Allow some variation in the way mods are specified.
+    modsStr = modsStr.ToLower().TrimStart('+').Replace(" ", "");
+
+    // Try to parse the mods by chunking the string into 2-character strings and then parsing them.
+    List<OsuMod> mods = new List<OsuMod>();
+    foreach (string acronym in modsStr.Chunk(2).Select(x => new string(x)))
+      if (OsuMod.All.Any(x => x.Acronym == acronym))
+        mods.Add(OsuMod.All.First(x => x.Acronym == acronym));
+
+    return mods.ToArray();
+  }
+}
