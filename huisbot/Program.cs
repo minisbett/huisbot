9382afe458f5d7ffbd73008b8fc05ad346d162a9
minisbett <39670899+minisbett@users.noreply.github.com>
2024-01-15 16:20:09 +0100

    Bump version

--- huisbot/Program.cs
+++ huisbot/Program.cs
@@ -19,7 +19,7 @@ public class Program
   /// <summary>
   /// The version of the application.
   /// </summary>
-  public const string VERSION = "2.0.2";
+  public const string VERSION = "2.0.3";
 
   /// <summary>
   /// The startup time of the application.
