c6bdbbfc01f60b4dbfdae9516925989602028bed
minisbett <39670899+minisbett@users.noreply.github.com>
2025-02-21 18:51:14 +0100

    refactor link command

--- huisbot/Modules/Link.cs
+++ huisbot/Modules/Link.cs
@@ -15,25 +15,13 @@ public class LinkCommandModule(IServiceProvider services) : ModuleBase(services)
 {
   [SlashCommand("link", "Links your Discord account to the specified osu! user by it's ID or name.")]
   public async Task LinkAsync(
-    [Summary("user", "The osu! ID or name of the player.")] string userId)
+    [Summary("user", "The osu! ID or name of the user.")] string userId)
   {
     await DeferAsync();
 
-    // Get the user from the osu! api. If it failed or the user could not be found, notify the user.
-    NotFoundOr<OsuUser>? user = await OsuApi.GetUserAsync(userId);
-    if (user is null)
-    {
-      await FollowupAsync(embed: Embeds.InternalError("Failed to resolve the user from the osu! API."));
-      return;
-    }
-    else if (!user.Found)
-    {
-      await FollowupAsync(embed: Embeds.Error($"No player with identifier `{userId}` could not be found."));
-      return;
-    }
+    if (await GetOsuUserAsync(userId) is not OsuUser user) return;
 
-    // Otherwise add/update the link in the database and notify the user about the change.
-    await Persistence.SetOsuDiscordLinkAsync(Context.User.Id, ((OsuUser)user).Id);
+    await Persistence.SetOsuDiscordLinkAsync(Context.User.Id, user.Id);
     await FollowupAsync(embed: Embeds.LinkSuccessful(user));
   }
 }
