8ff78c26742c4f5fc56e2a486902a2cc2636cdfe
minisbett <39670899+minisbett@users.noreply.github.com>
2025-08-29 00:09:02 +0200

    bump version

--- huisbot/Program.cs
+++ huisbot/Program.cs
@@ -24,7 +24,7 @@ public class Program
   /// <summary>
   /// The version of the application.
   /// </summary>
-  public const string VERSION = "2.9.1";
+  public const string VERSION = "2.9.2";
 
   public static async Task Main(string[] args)
   {
